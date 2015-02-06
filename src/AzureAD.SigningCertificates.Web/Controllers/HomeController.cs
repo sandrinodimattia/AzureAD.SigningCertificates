5735e132766a577a286969cb71f24daee5321f01
Sandrino Di Mattia <contact@sandrino.be>
2015-02-06 18:03:50 +0100

    Added web project

---
+++ src/AzureAD.SigningCertificates.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Metadata;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Security;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace AzureAD.SigningCertificates.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(GetSigningCertificates(ConfigurationManager.AppSettings["WsFederationMetadataUrl"]));
+        }
+
+
+        private static List<X509SecurityToken> GetSigningCertificates(string metadataAddress)
+        {
+            var tokens = new List<X509SecurityToken>();
+
+            using (var metadataReader = XmlReader.Create(metadataAddress))
+            {
+                var serializer = new MetadataSerializer { CertificateValidationMode = X509CertificateValidationMode.None };
+
+                var metadata = serializer.ReadMetadata(metadataReader) as EntityDescriptor;
+                if (metadata != null)
+                {
+                    var descriptior = metadata.RoleDescriptors.OfType<SecurityTokenServiceDescriptor>().First();
+                    if (descriptior != null)
+                    {
+                        var keys = descriptior.Keys
+                            .Where(key => key.KeyInfo != null && (key.Use == KeyType.Signing || key.Use == KeyType.Unspecified))
+                            .Select(key => key.KeyInfo.OfType<X509RawDataKeyIdentifierClause>().First());
+                        tokens.AddRange(keys.Select(token => new X509SecurityToken(new X509Certificate2(token.GetX509RawData()))));
+                    }
+                }
+            }
+
+            return tokens;
+        }
+    }
+}
---
+++ src/AzureAD.SigningCertificates.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AzureAD.SigningCertificates.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureAD.SigningCertificates.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("953dbebc-d925-49fd-b195-5a5696b756f6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
